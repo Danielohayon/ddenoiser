812b896ee8a20164d0bea076b8b5f6e0589668e4
danielohayon <danielohayon444@gmail.com>
2022-03-02 11:17:33 +0200

    added nore launch scripts

--- launch_deep_encoder_regular_lstm.sh
+++ launch_deep_encoder_regular_lstm.sh
@@ -1,9 +1,9 @@
 # here we will set the base line for a 1mil parameter model
 # Two layers of encoder with deep lstm
-#[2022-03-02 11:04:07,706][__main__][INFO] - Size: 3.9 MB
-#[2022-03-02 11:04:07,707][__main__][INFO] - num params 1.028554
-#[2022-03-02 11:04:07,707][__main__][INFO] - Field: 0.6 ms
-#[2022-03-02 11:04:07,708][__main__][INFO] - enc 0.136293, dec 0.136117, lstm 0.756144 in mil
+#[2022-03-02 10:30:58,827][__main__][INFO] - num params 1.115674
+#[2022-03-02 10:30:58,827][__main__][INFO] - Field: 0.6 ms
+#[2022-03-02 10:30:58,827][__main__][INFO] - enc 0.088341, dec 0.088213, lstm 0.93912 in mil
+
 python train.py \
   dset=valentini \
   demucs.causal=1 \
@@ -16,7 +16,7 @@ python train.py \
   stride=0.5 \
   restart=True \
   batch_size=128 \
-  demucs.depth=2 \
-  demucs.growth=3.7 \
-  demucs.lstm_depth=3 \
+  demucs.depth=8 \
+  demucs.growth=1.25 \
+  demucs.lstm_depth=2 \
   ddp=1 $@
